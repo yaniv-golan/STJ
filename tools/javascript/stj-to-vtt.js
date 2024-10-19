36a297fce7240339c5db26bb1379dab30c1bec7a
Yaniv Golan <yaniv@golan.name>
2024-10-20 01:59:22 +0300

    Create stj-to-vtt.js

---
+++ tools/javascript/stj-to-vtt.js
@@ -0,0 +1,55 @@
+#!/usr/bin/env node
+
+const fs = require('fs');
+const webvtt = require('node-webvtt');
+const moment = require('moment');
+const process = require('process');
+
+function loadSTJ(stjFilePath) {
+  const data = fs.readFileSync(stjFilePath, 'utf8');
+  return JSON.parse(data);
+}
+
+function formatTimestamp(seconds) {
+  const duration = moment.duration(seconds, 'seconds');
+  return moment.utc(duration.asMilliseconds()).format('HH:mm:ss.SSS');
+}
+
+function generateVTT(stjData, outputVttPath) {
+  const segments = stjData['transcript']['segments'];
+  const cues = [];
+  for (let i = 0; i < segments.length; i++) {
+    const seg = segments[i];
+    const start = formatTimestamp(seg['start']);
+    const end = formatTimestamp(seg['end']);
+    const speaker = seg['speaker_id'] || '';
+    const text = seg['text'];
+    const content = speaker ? `${speaker}: ${text}` : text;
+    cues.push({
+      identifier: '',
+      start: start,
+      end: end,
+      text: content,
+      styles: ''
+    });
+  }
+  const vttData = webvtt.compile({ cues: cues });
+  fs.writeFileSync(outputVttPath, vttData);
+  console.log(`WebVTT file generated: ${outputVttPath}`);
+}
+
+function main() {
+  const args = process.argv.slice(2);
+  if (args.length !== 2) {
+    console.error('Usage: stj-to-vtt.js <stj_file> <output_vtt>');
+    process.exit(1);
+  }
+  const stjFile = args[0];
+  const outputVtt = args[1];
+  const stjData = loadSTJ(stjFile);
+  generateVTT(stjData, outputVtt);
+}
+
+if (require.main === module) {
+  main();
+}
