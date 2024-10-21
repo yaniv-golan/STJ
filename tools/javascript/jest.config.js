f9e1a6aaa1fe5a717524fa83d494c50826ac20e3
yaniv-golan <yaniv@golan.name>
2024-10-21 14:01:02 +0300

    Update CI

--- tools/javascript/jest.config.js
+++ tools/javascript/jest.config.js
@@ -1,4 +1,4 @@
- module.exports = {
-    testEnvironment: 'node',
-    testMatch: ['**/tests/javascript/**/*.test.js'],
-  };
+module.exports = {
+  testEnvironment: 'node',
+  testMatch: ['**/tests/**/*.test.js'],
+};
