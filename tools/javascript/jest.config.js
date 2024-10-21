40aecebdc8bca5fce913a7fa001d3b5e7dd683a3
yaniv-golan <yaniv@golan.name>
2024-10-21 14:37:37 +0300

    Iterating on javascript tests

--- tools/javascript/jest.config.js
+++ tools/javascript/jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
+  rootDir: '../../', // Points to the project root
   testEnvironment: 'node',
-  testMatch: ['<rootDir>/../../tests/javascript/**/*.test.js'],
-  rootDir: __dirname,
+  testMatch: ['<rootDir>/tests/javascript/**/*.test.js'],
 };
